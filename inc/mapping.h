d0dd393f1d422b45fa581d508b15f80c00980d34
Anoj Perera <anojmperera@gmail.com>
2022-05-25 00:25:12 +0100

    Mapping class added
    This is not complete yet

---
+++ inc/mapping.h
@@ -0,0 +1,15 @@
+/*
+ * Mapping file for json-w mapping.
+ */
+#ifndef __MAPPING_H__
+#define __MAPPING_H__
+
+#include <stdlib.h>
+#include "buffer.h"
+
+struct mapping {
+  unsigned int init_flag;
+};
+
+
+#endif  /* __MAPPING_H__ */
---
+++ src/mapping.c
@@ -0,0 +1,10 @@
+/* This is mapping file for csvw */
+#import "../inc/buffer.h"
+
+struct mapping {
+        struct buffer *file_name;
+        struct buffer *stream;
+};
+
+
+
