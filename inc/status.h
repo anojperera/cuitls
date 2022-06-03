27ed5924b2a6dfa1f0dbc976b1a30002b4d27f5c
Anoj Perera <anojmperera@gmail.com>
2022-06-03 23:53:15 +0100

    Macro added to check if an object is initialised

--- inc/status.h
+++ inc/status.h
@@ -4,4 +4,8 @@
 #define CCSVCUBE_STATUS_SUCCESS 0
 #define CCSVCUBE_STATUS_FAILED -1
 
+#define CUTILS_CHECK_INIT(obj) \
+        if((obj)->init_flg != CCSVCUBE_STATUS_SUCCESS) \
+                return CCSVCUBE_STATUS_FAILED
+
 #endif /* __status_h__ */
