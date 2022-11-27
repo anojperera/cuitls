bbe30b9648f6a43b51587b156888f95748040b07
Anoj Perera <anojmperera@gmail.com>
2022-11-27 00:21:23 +0000

    Main updated

--- src/main.c
+++ src/main.c
@@ -1,62 +1,74 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "../../inc/buffer_types.h"
-#include "../../inc/buffer.h"
-#include "../../inc/reader.h"
-#include "../../inc/log.h"
-#include "../../inc/status.h"
-#include "../../inc/geojson.h"
+#include "../inc/buffer.h"
+#include "../inc/geojson.h"
+#include "../inc/log.h"
+#include "../inc/reader.h"
+#include "../inc/status.h"
 #include "json-c/json.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define FILE_PATH "./pipeswithcriticandcapa.geojson"
-int main(int argc, char** argv)
-{
-        struct buffer buf;
-        const char tmp_str[] = "This is a string";
-        json_object *jobj = NULL;
-        struct geojson geo;
+int geo_main(int argc, char **argv);
+int web_main(int argc, char **argv);
 
-        LOG_MESSAGE_ARGS("Number of arguments pased: %i", argc);
-        if (argc < 2) {
-                LOG_MESSAGE("Please pass the file name as an argument");
-                return CCSVCUBE_STATUS_FAILED;
-        }
+int main(int argc, char **argv) { return geo_main(argc, argv); }
 
-        LOG_MESSAGE("Starting test program");
-        /* initialise the buffer  */
+int geo_main(int argc, char **argv) {
+  struct buffer buf;
+  const char tmp_str[] = "This is a string";
+  json_object *jobj = NULL;
+  struct geojson geo;
 
-        LOG_MESSAGE("Testing buffer");
-        init_buffer(&buf, 20, buffer_type_str);
-        buffer_set_value(&buf, tmp_str, sizeof(tmp_str));
+  LOG_MESSAGE_ARGS("Number of arguments pased: %i", argc);
+  if (argc < 2) {
+    LOG_MESSAGE("Please pass the file name as an argument");
+    return CCSVCUBE_STATUS_FAILED;
+  }
 
-        destroy_buffer(&buf);
+  LOG_MESSAGE("Starting test program");
+  /* initialise the buffer  */
 
-        LOG_MESSAGE("Testing JSON reader");
-        if (read_json_file(argv[1], &buf, &jobj) == CCSVCUBE_STATUS_FAILED) {
-                LOG_MESSAGE("Failed to parse json");
-                destroy_buffer(&buf);
-                return CCSVCUBE_STATUS_FAILED;
-        }
+  LOG_MESSAGE("Testing buffer");
+  init_buffer(&buf, 20, buffer_type_str);
+  buffer_set_value(&buf, tmp_str, sizeof(tmp_str));
 
-        LOG_MESSAGE("Initialising geojson object");
-        geojson_init(&geo);
+  destroy_buffer(&buf);
 
-        if (argc > 2) {
-                geo.base_file = argv[2];
-        }
+  LOG_MESSAGE("Testing JSON reader");
+  if (read_json_file(argv[1], &buf, &jobj) == CCSVCUBE_STATUS_FAILED) {
+    LOG_MESSAGE("Failed to parse json");
+    destroy_buffer(&buf);
+    return CCSVCUBE_STATUS_FAILED;
+  }
 
-        if (argc > 3) {
-                geo.page_sz = atoi(argv[3]);
-        }
+  LOG_MESSAGE("Initialising geojson object");
+  geojson_init(&geo);
 
-        LOG_MESSAGE("Splitting into multiple geojson files");
-        geojson_split_into_multiples(&geo, jobj);
+  if (argc > 2) {
+    geo.base_file = argv[2];
+  }
 
-        geojson_free(&geo);
+  if (argc > 3) {
+    geo.page_sz = atoi(argv[3]);
+  }
 
-        LOG_MESSAGE("Complete Test");
+  LOG_MESSAGE("Splitting into multiple geojson files");
+  geojson_split_into_multiples(&geo, jobj);
 
-        destroy_buffer(&buf);
-        return CCSVCUBE_STATUS_SUCCESS;
+  geojson_free(&geo);
+
+  LOG_MESSAGE("Complete Test");
+
+  destroy_buffer(&buf);
+  return CCSVCUBE_STATUS_SUCCESS;
+}
+
+int web_main(int argc, char **argv) {
+  char *uri = argv[1];
+
+  LOG_MESSAGE("Starting WEB URI Program");
+
+  return CCSVCUBE_STATUS_SUCCESS;
 }
