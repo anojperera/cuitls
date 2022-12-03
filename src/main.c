d56330a8d6e6baa32c5f5ab65e4525e19a0eec8a
Anoj Perera <anojmperera@gmail.com>
2022-12-03 22:56:26 +0000

    Updated submodules and updated CMake

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "../inc/reader.h"
 #include "../inc/status.h"
 #include "../inc/writer.h"
+#include "http.h"
 #include "json-c/json.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,10 +14,36 @@
 #define FILE_PATH "./pipeswithcriticandcapa.geojson"
 int geo_main(int argc, char **argv);
 int web_main(int argc, char **argv);
+// We'll use this callback in `http_listen`, to handles HTTP requests
+void on_request(http_s *request);
 
-int main(int argc, char **argv)
+// These will contain pre-allocated values that we will use often
+FIOBJ HTTP_X_DATA;
+
+// Listen to HTTP requests and start facil.io
+int main(int argc, char const **argv)
 {
-        return web_main(argc, argv);
+        // allocating values we use often
+        HTTP_X_DATA = fiobj_str_new("X-Data", 6);
+        // listen on port 3000 and any available network binding (NULL ==
+        // 0.0.0.0)
+        http_listen("3000", NULL, .on_request = on_request, .log = 1);
+
+        fio_start(.threads = 1);
+
+        // deallocating the common values
+        fiobj_free(HTTP_X_DATA);
+}
+
+// Easy HTTP handling
+void on_request(http_s *request)
+{
+        http_set_cookie(request, .name = "my_cookie", .name_len = 9,
+                        .value = "data", .value_len = 4);
+        http_set_header(request, HTTP_HEADER_CONTENT_TYPE,
+                        http_mimetype_find("txt", 3));
+        http_set_header(request, HTTP_X_DATA, fiobj_str_new("my data", 7));
+        http_send_body(request, "Hello World!\r\n", 14);
 }
 
 int geo_main(int argc, char **argv)
